cf7a2ffaf43440204a493b0c0133c8455c86488d
Mark Petrie <petrie.mark@gmail.com>
2017-08-03 17:44:09 -0700

    Final commit with actual changes :)

--- tests/e2e/restaurants.test.js
+++ tests/e2e/restaurants.test.js
@@ -6,13 +6,21 @@ describe('restaurant REST api', () => {
 
     before(db.drop);
 
-    let validRest = {
+    let validRest1 = {
         name: 'Asian Gardens',
         address: {
             street: 'Burnside',
             city: 'Portland'
         },
-        cuisine: ['Asian']
+        cuisine: ['asian']
+    };
+    let validRest2 = {
+        name: 'Euro Gardens',
+        address: {
+            street: 'Powell',
+            city: 'Portland'
+        },
+        cuisine: ['euro']
     };
 
     let noName = {
@@ -21,7 +29,7 @@ describe('restaurant REST api', () => {
             street: 'Burnside',
             city: 'Portland'
         },
-        cuisine: ['Asian']
+        cuisine: ['asian']
     };
     let noCuisine = {
         name: 'Comfort Gardens',
@@ -37,15 +45,41 @@ describe('restaurant REST api', () => {
             street: 'Burnside',
             city: 'Portland'
         },
-        cuisine: ['Mexican']
+        cuisine: ['mexican']
     };
 
     function saveRestaurant(restaurant) {
-
-        return request.post('/restaurants')
+        return request.post('/restaurant')
             .send(restaurant)
             .then(res => res.body);
     }
+
+    it('returns both restaurants', () => {
+        saveRestaurant(validRest1),
+        saveRestaurant(validRest2)
+
+            .then(savedRestaurants => {
+                validRest1 = savedRestaurants[0];
+                validRest2 = savedRestaurants[1];
+            })
+            .then(() => request.get('/restaurants'))
+            .then(res => res.body)
+            .then(restaurants => {
+                assert.equal(restaurants.length, 2);
+                assert.deepInclude(restaurants, validRest1);
+                assert.deepInclude(restaurants, validRest2);
+            });
+    });
+
+    it('gets restaurant by id', () => {
+        return request.get(`/restaurants/${validRest1._id}`)
+
+            .then(res => res.body)
+            .then(got => {
+                assert.deepEqual(got, validRest1);
+                assert.equal(got.name, 'Asian Gardens');
+            });
+    });
 
     it('errors on validation failure', () => {
         return saveRestaurant(noName)
